3fb7ef363a2adf03d9aafbca630438773145fd41
Nikhil Mangali <nikhilmangali158@gmail.com>
2025-07-16 09:35:51 +0530

    first commit

---
+++ src/main/java/com/nikhilmangali1/CourseSearchService/CourseSearchServiceApplication.java
@@ -0,0 +1,15 @@
+package com.nikhilmangali1.CourseSearchService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSearchServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CourseSearchServiceApplication.class, args);
+		System.out.println("CourseSearchService is running...");
+	}
+
+}
---
+++ src/test/java/com/nikhilmangali1/CourseSearchService/CourseSearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhilmangali1.CourseSearchService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSearchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
